e94b6d66a71bb6e20cfef328280f174c9c29baf4
Nathan Wilson <91117438+N-Wilson11@users.noreply.github.com>
2021-11-04 16:57:56 +0100

    Add files via upload

--- Code_Simon.ino
+++ Code_Simon.ino
@@ -2,46 +2,46 @@
 #define TRUE 1
 
 // Safe to Arduino pin mapping
-#define PIN_LED_BLUE 2
-#define PIN_LED_GREEN 9
-#define PIN_LED_RED 4
-#define PIN_LED_YELLOW 7
+#define LED_BLAUW 2
+#define LED_GROEN 9
+#define LED_ROOD 4
+#define LED_GEEL 7
 
 #define PIN_BTN_LOW 8
-#define PIN_BTN_BLUE 3
-#define PIN_BTN_GREEN 11
-#define PIN_BTN_RED 5
-#define PIN_BTN_YELLOW 13
+#define KNOP_BLAUW 3
+#define KNOP_GROEN 11
+#define KNOP_ROOD 5
+#define KNOP_GEEL 13
 
-#define PIN_BUZZER A0
+#define BUZZER A0
 
 typedef struct {
-  int button;
-  int led;
-  int buzzerFrequency;
+  int knop;
+  int LED;
+  int buzzerHoogte;
 } ButtonLed;
 
 const ButtonLed BUTTON_LEDS[4] = {
-  {PIN_BTN_BLUE, PIN_LED_BLUE, 400},
-  {PIN_BTN_GREEN, PIN_LED_GREEN, 800},
-  {PIN_BTN_RED, PIN_LED_RED, 1200},
-  {PIN_BTN_YELLOW, PIN_LED_YELLOW, 1600},
+  {KNOP_BLAUW, LED_BLAUW, 1000},
+  {KNOP_GROEN, LED_GROEN, 800},
+  {KNOP_ROOD, LED_ROOD, 600},
+  {KNOP_GEEL, LED_GEEL, 400},
 };
-int code[2];
+int code[12];
 int codeLength = 0;
 int n;
 
 void setup() {
-  pinMode(PIN_LED_BLUE, OUTPUT);
-  pinMode(PIN_LED_GREEN, OUTPUT);
-  pinMode(PIN_LED_RED, OUTPUT);
-  pinMode(PIN_LED_YELLOW, OUTPUT);
-  pinMode(PIN_BUZZER, OUTPUT);
+  pinMode(LED_BLAUW, OUTPUT);
+  pinMode(LED_GROEN, OUTPUT);
+  pinMode(LED_ROOD, OUTPUT);
+  pinMode(LED_GEEL, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
 
-  pinMode(PIN_BTN_BLUE, INPUT_PULLUP);
-  pinMode(PIN_BTN_GREEN, INPUT_PULLUP);
-  pinMode(PIN_BTN_RED, INPUT_PULLUP);
-  pinMode(PIN_BTN_YELLOW, INPUT_PULLUP);
+  pinMode(KNOP_BLAUW, INPUT_PULLUP);
+  pinMode(KNOP_GROEN, INPUT_PULLUP);
+  pinMode(KNOP_ROOD, INPUT_PULLUP);
+  pinMode(KNOP_GEEL, INPUT_PULLUP);
 
   pinMode(PIN_BTN_LOW, OUTPUT);
   digitalWrite(PIN_BTN_LOW, LOW);
@@ -52,7 +52,7 @@ void setup() {
 
 
 void soundBuzzer(unsigned int frequency, unsigned int duration, int wait) {
-  tone(PIN_BUZZER, frequency, duration);
+  tone(BUZZER, frequency, duration);
   if (wait) {
     delay(duration);
   }
@@ -66,7 +66,7 @@ void showLed(unsigned int pin, unsigned int duration) {
 
 int buttonPressed() {
   for (int i = 0; i < 4; ++i) {
-    if (digitalRead(BUTTON_LEDS[i].button) == LOW) {
+    if (digitalRead(BUTTON_LEDS[i].knop) == LOW) {
       return i;
     }
   }
@@ -74,11 +74,11 @@ int buttonPressed() {
   return -1;
 }
 
-void showCode() {
+void show_order() {
   for (int i = 0; i < codeLength; i++) {
     ButtonLed bl = BUTTON_LEDS[code[i]];
-    soundBuzzer(bl.buzzerFrequency, 500, FALSE);
-    showLed(bl.led, 500);
+    soundBuzzer(bl.buzzerHoogte, 500, FALSE);
+    showLed(bl.LED, 500);
   }
 }
 
@@ -88,7 +88,7 @@ void generateNextNumber() {
     code[codeLength] = random() % 4;
   } while (codeLength > 1 && code[codeLength] == code[codeLength - 1] && code[codeLength] == code[codeLength - 2]);
   codeLength++;
-  showCode();
+  show_order();
   n = 0;
 }
 
@@ -101,8 +101,8 @@ void loop() {
       generateNextNumber();
     } else {
       ButtonLed bl = BUTTON_LEDS[pressed];
-      soundBuzzer(bl.buzzerFrequency, 300, FALSE);
-      showLed(bl.led, 300);
+      soundBuzzer(bl.buzzerHoogte, 300, FALSE);
+      showLed(bl.LED, 300);
 
       if (code[n] == pressed) {
         // Guessed code digit correctly
